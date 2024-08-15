d2acb2b10b5b39338efeaf538bddd22865121e05
Juan Lopez <juanpablolopez43@gmail.com>
2024-08-15 12:05:32 -0300

    google analytics add

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,16 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
+import partytown from '@astrojs/partytown';
 
 import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), react()]
+  integrations: [mdx(), sitemap(), tailwind(), react(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    },
+}),]
 });
