7b96d74a7bf74250723b6903360d8813d5d40518
Juan Lopez <juanpablolopez43@gmail.com>
2024-11-06 17:15:41 -0300

    fix icon logo

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -1,9 +1,13 @@
 
 import Swal from 'sweetalert2'
-
+import CyberMonay from '/src/images/CyberMonday.jpg'
+import {Image} from 'astro:assets'
 const PopUp = () => {
+  const imagePopUp = <>
+  <Image src={CyberMonay} alt='CyberMonday'/>
+  </>
   Swal.fire({
-    html: '<img src=./src/images/CyberMonday.jpg />',
+    html: `${imagePopUp}`, 
     showConfirmButton: false,
     showCloseButton: true,
 
