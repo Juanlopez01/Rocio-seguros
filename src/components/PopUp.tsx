777044bbdc6addc87277840620100d7defee9025
Juan Lopez <juanpablolopez43@gmail.com>
2024-12-04 16:16:13 -0300

    change promos popup

--- src/components/PopUp.tsx
+++ src/components/PopUp.tsx
@@ -3,11 +3,20 @@ import Swal from 'sweetalert2'
 
 const PopUp = () => {
   Swal.fire({
-    html: '<img src=./img/cyberMonday.jpg />',
+    html: '<img src=./img/30offauto.jpeg />',
     showConfirmButton: false,
     showCloseButton: true,
-
-  })
+    timer: 3000,
+    timerProgressBar: true,
+  }).then(() => {
+    Swal.fire({
+      html: '<img src=./img/50offyauto.jpeg />',
+      showConfirmButton: false,
+      showCloseButton: true,
+      timer: 3000,
+      timerProgressBar: true,
+    })}
+  );
   return (
     <> </>
   )
